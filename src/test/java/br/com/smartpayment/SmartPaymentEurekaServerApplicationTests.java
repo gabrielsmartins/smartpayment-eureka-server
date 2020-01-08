919c4b95c5af32b4e8c5158ea5a04c1e2f4461b6
Gabriel Martins <ga.smartins94@gmail.com>
2020-01-07 22:18:41 -0300

    First Commit

---
+++ src/test/java/br/com/smartpayment/SmartPaymentEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPaymentEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
